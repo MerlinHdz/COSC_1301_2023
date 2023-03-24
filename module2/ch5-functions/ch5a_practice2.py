abd68d23d5fef0b90a75d9dc1455a416f28dcf03
Merlin Hernandez <mh@Merlins-Air.lan>
2023-03-24 15:54:03 -0500

    Added chapter5-a practice 1 and 2

---
+++ module2/ch5-functions/ch5a_practice2.py
@@ -0,0 +1,21 @@
+# Merlin Hernandez (55096)
+# Program Complete
+"""
+This program will convert a distance in kilometers to miles. It will get a value in kilometers from 
+the user, and call a function to do the conversion. Finally, it will display the value.
+"""
+# Global constant for km/mile conversion factor
+KM_TO_MI_FACTOR = 0.6214
+
+def main():
+    km_value = float(input("Enter a value in kilometers: "))
+    km_to_miles(km_value)
+
+# this function takes a value in kilometers, converts it to miles and prints the new value.
+def km_to_miles(kilometers):
+    # Using an f string, format the value to have two decimal places.
+    print(f"{(kilometers * KM_TO_MI_FACTOR):.2f} Miles")
+
+
+# Call main function to start the program
+main()
