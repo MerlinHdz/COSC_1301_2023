5f6c8279868e3c0e8c4955bf830f1bbf54b3170d
Merlin Hernandez <mh@Merlins-Air.lan>
2023-03-24 15:55:08 -0500

    Modified ch5a practice 1

--- module2/ch5-functions/ch5a_practice1.py
+++ module2/ch5-functions/ch5a_practice1.py
@@ -1,20 +1,61 @@
-# Merlin Hernandez
+# Merlin Hernandez (55096)
 # Program Complete
 '''
 This program will display the steps for making a sandwich
 '''
 
 def main():
-    pass
+    # Display a starting message
+    starting_message()
 
-def greet_user():
-    print("This is how you make a sandwich")
+    input("Press Enter to see the first step ")
+    # step1
+    step1()
+
+    input("Press Enter to see the next step ")
+    # step 2
+    step2()
+
+    input("Press Enter to see the next step ")
+    # step 3
+    step3()
+
+    input("Press Enter to see the next step ")
+    # step 4
+    step4()
+
+    input("Press Enter to see the final step ")
+    # step 5
+    step5()
+
+
+# Function to display a message at the beginning of the program
+def starting_message():
+    print("This program tells you how to prepare a peanut butter and jelly sandwich")
+    print("There are five easy steps.")
+    print()
 
 def step1():
-    print("Step 1: Gather your ingredients")
+    print("Step 1: Gather your ingredients and supplies")
+    print("Bread, peanut butter, jelly, a butter knife, a plate")
+    print()
 
 def step2():
-    print("Step 2: Grab a slice of bread and set it on a flat surface")
+    print("Step 2: Grab two slices of bread and place them on a plate")
+    print()
 
 def step3():
-    pass
+    print("Step 3: Using the butter knife, spread peanut butter on a slice of bread")
+    print()
+
+def step4():
+    print("Step 4: Using your knife, now spread jelly on the other slice of bread")
+    print()
+
+def step5():
+    print("Step 5: Assemble the sandwich by placing the peanut butter and jelly sides together.")
+    print()
+
+
+# Call the main function to start the program
+main()
